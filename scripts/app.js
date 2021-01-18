466de6b7da5cc21638e1e77a3fcd9ed8293662cb
damiloyola <damiloyola@gmail.com>
2021-01-18 16:39:40 -0300

    creo pagina de detalles para cada heroe cuando se le hace click

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,84 @@
-const baseURL =
-    "https://gateway.marvel.com:443/v1/public/characters?orderBy=name&ts=1&apikey=f8b23396eb3de69e79a66091fe7b29c0&hash=addc65eb95126540e2c9889f0632a6ef";
+//**** DOM ****//
+const heroesContainer = $("#heroesContainer");
+const heroesUl = $("#heroesUl");
+const heroeDetails = $("#heroeDetails");
 
+//**** Funcion para hacer llamado a la api ****//
 const fetchApi = async (endpoint) => {
-    await fetch(endpoint)
-        .then((response) => response.json())
-
-        .then((data) => showAllHeroes(data.data.results));
+    const res = await fetch(endpoint);
+    return await res.json();
 };
 
-const showAllHeroes = (heroes) => {
-    const container = $("#heroesContainer");
+//**** Funcion para mostrar todos los resultados ***//
+const showAllHeroes = async () => {
+    const url =
+        "https://gateway.marvel.com:443/v1/public/characters?orderBy=name&ts=1&apikey=f8b23396eb3de69e79a66091fe7b29c0&hash=addc65eb95126540e2c9889f0632a6ef";
+    const heroes = await fetchApi(url);
 
-    console.log(heroes);
-    return heroes.map((heroe) =>
-        container.append(`
-    
-    <li>
-        
-            <img src="${heroe.thumbnail.path}.${heroe.thumbnail.extension}"/>
-            <h5> ${heroe.name}</h5>
-        
-   
-    </li>
-    
-    `)
+    heroes.data.results.map((heroe) =>
+        heroesUl.append(`
+
+        <li onclick="showHeroe(${heroe.id})">
+
+                <img src="${heroe.thumbnail.path}.${heroe.thumbnail.extension}" alt=${heroe.name}/>
+                <h5> ${heroe.name}</h5>
+
+        </li>
+
+        `)
     );
 };
 
-fetchApi(baseURL);
+//**** Funcion que muestra un heroe en particular ****//
+const showHeroe = async (id) => {
+    const url = `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=f8b23396eb3de69e79a66091fe7b29c0&hash=addc65eb95126540e2c9889f0632a6ef`;
+    const result = await fetchApi(url);
+    heroe = result.data.results[0];
+    console.log(heroe);
+    heroesContainer.toggleClass("hidden");
+    heroeDetails.toggleClass("hidden");
+
+    const getComics = async () => {
+        let comics = [];
+        let length;
+        heroe.comics.available < 8
+            ? (length = heroe.comics.available)
+            : (length = 8);
+
+        for (let i = 0; i < length; i++) {
+            const url =
+                heroe.comics.items[i].resourceURI +
+                "?ts=1&apikey=f8b23396eb3de69e79a66091fe7b29c0&hash=addc65eb95126540e2c9889f0632a6ef";
+            const comic = await fetchApi(url);
+            const comicURL = `${comic.data.results[0].images[0].path}.${comic.data.results[0].images[0].extension}`;
+            await comics.push(comicURL);
+        }
+        console.log(comics);
+        return comics;
+    };
+
+    heroeDetails.append(`
+        <div>
+            <h3>${heroe.name}</h3>
+            <div>
+                <img src="${heroe.thumbnail.path}.${
+        heroe.thumbnail.extension
+    }"  alt="${heroe.name}"/>
+                <div>   
+                    ${heroe.description != " " && `<p>${heroe.description}</p>`}
+                    <p>Aparece en:</p>
+                    <div>
+                       ${(await getComics()).map(
+                           (comicImg) => `<img src="${comicImg}"/>`
+                       )}
+                    </div>
+                </div>
+                <button>Volver al Inicio </button>
+                <button>Agregar a Favorito</button>
+            </div>
+            
+        </div>
+    `);
+};
+
+showAllHeroes();
