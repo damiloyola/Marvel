ea652f13761249075011fc1a1e4ba4b44823f2c4
damiloyola <damiloyola@gmail.com>
2021-01-18 18:54:43 -0300

    termino estilos de descripcion de heroe

--- scripts/app.js
+++ scripts/app.js
@@ -31,13 +31,15 @@ const showAllHeroes = async () => {
 
 //**** Funcion que muestra un heroe en particular ****//
 const showHeroe = async (id) => {
+    heroeDetails.innerHTML = " ";
     const url = `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=f8b23396eb3de69e79a66091fe7b29c0&hash=addc65eb95126540e2c9889f0632a6ef`;
     const result = await fetchApi(url);
+
     heroe = result.data.results[0];
     console.log(heroe);
     heroesContainer.toggleClass("hidden");
     heroeDetails.toggleClass("hidden");
-
+    /*Funcion que trae las img de comics donde aparece*/
     const getComics = async () => {
         let comics = [];
         let length;
@@ -53,32 +55,43 @@ const showHeroe = async (id) => {
             const comicURL = `${comic.data.results[0].images[0].path}.${comic.data.results[0].images[0].extension}`;
             await comics.push(comicURL);
         }
-        console.log(comics);
+
         return comics;
     };
+    const comics = await getComics();
 
     heroeDetails.append(`
-        <div>
+        <div class="heroeDetails">
             <h3>${heroe.name}</h3>
-            <div>
+            <div class="detailsContainer">
+                
                 <img src="${heroe.thumbnail.path}.${
         heroe.thumbnail.extension
     }"  alt="${heroe.name}"/>
-                <div>   
+                <div class="heroeDescription">   
                     ${heroe.description != " " && `<p>${heroe.description}</p>`}
-                    <p>Aparece en:</p>
-                    <div>
-                       ${(await getComics()).map(
-                           (comicImg) => `<img src="${comicImg}"/>`
-                       )}
+                    <h4>Aparece en:</h4>
+                    <div class="comicsContainer">
+                       ${comics.map((comicImg) => `<img src="${comicImg}"/>`)}
+                    </div>
+                
+                    <div class="detailsButtons">
+                            <button onclick="goHome()"}>Volver al Inicio </button>
+                            <button>Agregar a Favorito</button>
                     </div>
                 </div>
-                <button>Volver al Inicio </button>
-                <button>Agregar a Favorito</button>
+                
             </div>
             
         </div>
     `);
+};
+
+const goHome = () => {
+    window.scrollTo({ top: 0, behavior: "auto" });
+    heroeDetails.html("");
+    heroesContainer.toggleClass("hidden");
+    heroeDetails.toggleClass("hidden");
 };
 
 showAllHeroes();
