170c85c6caf5fd587ab1034210c7a628fdf36fa4
damiloyola <damiloyola@gmail.com>
2021-01-18 01:25:57 -0300

    traigo datos de api y los muestro en pantalla

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+const baseURL =
+    "https://gateway.marvel.com:443/v1/public/characters?orderBy=name&ts=1&apikey=f8b23396eb3de69e79a66091fe7b29c0&hash=addc65eb95126540e2c9889f0632a6ef";
+
+const fetchApi = async (endpoint) => {
+    await fetch(endpoint)
+        .then((response) => response.json())
+
+        .then((data) => showAllHeroes(data.data.results));
+};
+
+const showAllHeroes = (heroes) => {
+    const container = $("#heroesContainer");
+
+    console.log(heroes);
+    return heroes.map((heroe) =>
+        container.append(`
+    
+    <li>
+        <div>
+            <img src="${heroe.thumbnail.path}.${heroe.thumbnail.extension}"/>
+            <h5> ${heroe.name}</h5>
+        </div>
+   
+    </li>
+    
+    `)
+    );
+};
+
+fetchApi(baseURL);
